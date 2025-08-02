34ec7b5ed6730b8a3b8694570141420f80fb75ec
HT <huynhtrancntt@gmai.com>
2025-08-02 13:13:02 +0700

    update server

---
+++ update_server/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_from_directory('files', filename, as_attachment=True)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
+
+http: // localhost: 8000/download/update_v1.3.0.zip
